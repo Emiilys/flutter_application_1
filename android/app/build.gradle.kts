148b5327cc6387c5b68fe3775b1750b7dd65e680
mcmatozo <mcmatozo>
2025-09-18 15:24:45 -0300

    firestore e coleções

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,3 +42,8 @@ android {
 flutter {
     source = "../.."
 }
+
+id("com.android.application")
+id("com.google.gms.google-services")
+implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+implementation("com.google.firebase:firebase-analytics")
