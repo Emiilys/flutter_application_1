41d9a4fa202995d03b980427ebf00316af442e46
Emiilys <cristinabcemily@gmail.com>
2025-10-10 17:26:35 -0300

    ultima versão +firebase funcionado

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Import Firebase Core
+import 'package:flutter_application_1/firebase_options.dart';
 import 'loginpage.dart';
 
 void main() async {
  WidgetsFlutterBinding.ensureInitialized();
- // flawait Firebase.initializeApp();//** // 🔹 usa o google-services.json
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);//** // 🔹 usa o google-services.json
+  await FirebaseAuth.instance.createUserWithEmailAndPassword(email: "emily@gmail.com", password: "1234a00"); // 🔹 usa o emulador
   runApp(const MyApp());
+  
 }
 
 class MyApp extends StatelessWidget {
